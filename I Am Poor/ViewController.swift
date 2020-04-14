a4d7b9acd49c9c54142b6affe4957ddffb5650e5
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-04-14 07:42:28 -0400

    files for I Am Poor iOS app

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Taylor Patterson on 4/13/20.
+//  Copyright © 2020 Taylor Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
